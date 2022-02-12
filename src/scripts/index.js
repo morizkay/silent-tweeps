dccc097ba9c5bda902601f618a03555b363449fc
morizkay <morizkay@live.com>
2022-02-12 18:32:56 +0800

    Init

---
+++ src/scripts/index.js
@@ -0,0 +1,8 @@
+const silencer = () => {
+	const targetElement = document.getElementsByClassName(
+		'css-18t94o4 css-1dbjc4n r-1777fci r-bt1l66 r-1ny4l3l r-bztko3 r-lrvibr',
+	);
+	console.log({ targetElement: targetElement.length });
+};
+
+silencer();
