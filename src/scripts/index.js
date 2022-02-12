ead5f35bdf129a2da016c1f62f727501c42eaaf3
morizkay <morizkay@live.com>
2022-02-12 18:52:09 +0800

    Error

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,11 @@
-const silencer = () => {
-	const targetElement = document.getElementsByClassName(
-		'css-18t94o4 css-1dbjc4n r-1777fci r-bt1l66 r-1ny4l3l r-bztko3 r-lrvibr',
-	);
-	console.log({ targetElement: targetElement.length });
+const silencer = (targetClass) => {
+	const targetElement = document.getElementsByClassName(targetClass);
+	// Hide all targetElement
+	for (let i = 0; i < targetElement.length; i++) {
+		targetElement[i].style.display = 'none';
+	}
 };
 
-silencer();
+silencer(
+	'css-18t94o4 css-1dbjc4n r-1777fci r-bt1l66 r-1ny4l3l r-bztko3 r-lrvibr',
+);
